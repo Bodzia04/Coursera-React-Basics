4ca6c6f25227400dce88a189c627438e790226a9
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2024-12-16 16:44:09 +0100

    Week 1 -> React Components -> 2) React Components and Where They Live -> 12) Creating and importing components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Heading from './Heading'
 
-function Heading(){
-  return <h1>This is h1 heading</h1>
-}
+// function Heading(){
+//   return <h1>This is h1 heading</h1>
+// }
 function App() {
   return <Heading />
 }
