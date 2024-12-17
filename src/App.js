2c271458374ea95bdba419d0238bfe5ee7bf0d7b
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2024-12-17 19:12:08 +0100

    Week 1 -> React Components -> 3) Component Use and Styling -> 16) Multiple components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import Heading from './Heading'
+import Card from './Card';
 
-// function Heading(){
-//   return <h1>This is h1 heading</h1>
-// }
 function App() {
   return (
-    <div className = 'App'>
-      <Heading firstName = 'Bob' />
-      <Heading firstName = 'Betty' />
+    <div className='App'>
+    <h1>Task: Add three Card elements</h1>
+    <Card h2 = "First card's h2" h3 = "First card's h3"/>
+    <Card h2 = "Second card's h2" h3 = "Second card's h3"/>
+    <Card h2 = "Third card's h2" h3 = "Third card's h3"/>
     </div>
   )
 }
