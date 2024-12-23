612ce9043702eb3ed602086caddbd180e1674f2e
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2024-12-23 16:52:21 +0100

    Week 3 -> 1)Navigation, Updating and Assets in React.js -> 2) Using Assets in React -> 10) Song selection(написаний код може бути не корекний з тим що хоче задача)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from "react";
 import './App.css';
-import logo from './assets/Bentley.jpg'
+import ReactPlayer from 'react-player';
+import soundNumb from './assets/sounds/Linkin Park – Numb.mp3';
+import soundEye from './assets/sounds/Survivor – Eye of the Tiger.mp3';
 
 function App(){
+  const sound1 = new Audio(soundNumb);
+  const sound2 = new Audio(soundEye);
+
+  function toggle1(){
+    if(sound1.paused){
+      sound1.play();
+    } else {
+      sound1.pause()
+    }
+  }
+
+  function toggle2(){
+    if(sound2.paused){
+      sound2.play();
+    } else {
+      sound2.pause()
+    }
+  }
+
   return(
-    <div className="App">
-      <h1>Bentley</h1>
-      <img src={logo} alt="logo"></img>
+    <div>
+      <button onClick={toggle1}>Sound 1</button>
+      <button onClick={toggle2}>Sound 2</button>
     </div>
   )
 }
-
 export default App;
