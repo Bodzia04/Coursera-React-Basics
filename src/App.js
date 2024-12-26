d2d2ef17994293fc6041a6e9285597499c440e5d
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2024-12-26 11:34:56 +0100

    Week 4 -> 1) Your first React app -> 1) Graded Assessment Calculator app ->3) Build a calculator app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
-import React from "react";
+import {useState, useRef} from 'react';
 import './App.css';
-import ReactPlayer from 'react-player';
-import soundNumb from './assets/sounds/Linkin Park – Numb.mp3';
-import soundEye from './assets/sounds/Survivor – Eye of the Tiger.mp3';
 
 function App(){
-  const sound1 = new Audio(soundNumb);
-  const sound2 = new Audio(soundEye);
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
 
-  function toggle1(){
-    if(sound1.paused){
-      sound1.play();
-    } else {
-      sound1.pause()
-    }
-  }
+  function plus(e){
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  };
 
-  function toggle2(){
-    if(sound2.paused){
-      sound2.play();
-    } else {
-      sound2.pause()
-    }
-  }
+  function mines(e){
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+
+  function times(e){
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  };
+
+  function divide(e){
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+
+  function resetInput(e){
+    e.preventDefault();
+    inputRef.current.value = 0;
+  };
+  
+  function resetResult(e){
+    e.preventDefault();
+    setResult((prevVal) => prevVal * 0);
+  };
 
   return(
-    <div>
-      <button onClick={toggle1}>Sound 1</button>
-      <button onClick={toggle2}>Sound 2</button>
+    <div className='App'>
+      <div>
+        <h1>Simple Working Calculator</h1> 
+      </div>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input ref={inputRef} type='number' placeholder='Type a number'></input>
+        <button onClick={plus}>plus</button>
+        <button onClick={mines}>mines</button>
+        <button onClick={times}>times</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset resalt</button>
+      </form>
     </div>
   )
+
 }
+
 export default App;
